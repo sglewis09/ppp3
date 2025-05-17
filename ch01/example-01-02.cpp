8d4a9d84ff8170e60f9f8a73d1edf0edbde7ebb2
Stephen G. Lewis <sglewis@sglewis.com>
2025-05-17 19:02:46 -0400

    Initial checkin

---
+++ ch01/example-01-02.cpp
@@ -0,0 +1,13 @@
+// Programming: Principles and Practice Using C++ - 3rd Edition
+// by Bjarne Stroustrup
+// 
+// Example 1.2 - page 18
+// Outputs the message "Hello World!" to the screen
+
+import std;
+
+int main()		// C++ programs start by executing the function main
+{
+	std::cout << "Hello, World!\n";		// output Hello World!
+	return 0;
+}
