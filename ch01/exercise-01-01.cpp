5ec303704ffbd864acfb4acd117fc4a66d01d16a
Stephen G. Lewis <sglewis@sglewis.com>
2025-05-17 20:20:19 -0400

    Added exercise 1.1

---
+++ ch01/exercise-01-01.cpp
@@ -0,0 +1,17 @@
+// Programming: Principles and Practice Using C++ - 3rd Edition
+// by Bjarne Stroustrup
+// 
+// Exercise 1.2 - page 26
+// Outputs the following message to the screen:
+// Hello, programming!
+// Here we go!
+
+//--- Comment out and use include until If figure how to compile with modules
+// import std;
+#include "PPPheaders.h"
+
+int main()		// C++ programs start by executing the function main
+{
+	std::cout << "Hello, World!\nHere we go!\n";	// output message
+	return 0;
+}
