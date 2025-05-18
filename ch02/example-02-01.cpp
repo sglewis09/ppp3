6d4dc4c53d835d341336ada2b8353b0a524a4d03
Stephen G. Lewis <sglewis@sglewis.com>
2025-05-17 20:40:22 -0400

    Added example-02-01

---
+++ ch02/example-02-01.cpp
@@ -0,0 +1,19 @@
+// Programming: Principles and Practice Using C++ - 3rd Edition
+// by Bjarne Stroustrup
+// 
+// Example 2.1 - page 30
+// Rread and write a first name
+
+//--- Comment out and use include until If figure how to compile with modules
+// import std;
+#include "PPPheaders.h"
+
+int main()
+{
+	cout << "Please enter your first name (followed by \"enter\"):\n";
+
+	string first_name;	// first_name is a variable of type string
+	cin >> first_name;	// read characters into first_name
+	
+	cout << "Hello, " << first_name << "!\n";
+}
