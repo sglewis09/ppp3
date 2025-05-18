3af4ac86a64ebaa46e6852a00be05cc4562c27ce
Stephen G. Lewis <sglewis@sglewis.com>
2025-05-18 11:32:07 -0400

    Added example 02-03-01.cpp

---
+++ ch02/example-02-03-01.cpp
@@ -0,0 +1,23 @@
+// Programming: Principles and Practice Using C++ - 3rd Edition
+// by Bjarne Stroustrup
+// 
+// Example 2.3 - page 32
+// Read and write first name and age
+
+//--- Comment out and use include until If figure how to compile with modules
+// import std;
+#include "PPPheaders.h"
+
+int main()
+{
+	cout << "Please enter your first name and age\n";
+
+	string first_name = "???";	// string variable ("???" indicates
+								// "don't know the naem")
+	int age = -1;				// integer variable (-1 means
+								// "don't know the age
+
+	cin >> first_name >> age;	// read string followed by an integer
+	
+	cout << "Hello, " << first_name << " (age " << age << ")\n";
+}
