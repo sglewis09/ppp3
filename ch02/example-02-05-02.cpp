c30d0d841db35b8d4b6773491185d0192e8c15fa
Stephen G. Lewis <sglewis@sglewis.com>
2025-05-19 13:48:14 -0400

    Added example-02-05-02.cpp

--- ch02/example-02-05-02.cpp
+++ ch02/example-02-05-02.cpp
@@ -1,7 +1,7 @@
 // Programming: Principles and Practice Using C++ - 3rd Edition
 // by Bjarne Stroustrup
 // 
-// Example 2.4.1 - page 38
+// Example 2.5.3 - page 40
 // detect repeated words
 
 //--- Comment out and use include until If figure how to compile with modules
@@ -10,13 +10,18 @@
 
 int main()
 {
+	int number_of_words = 0;
+
 	// strings are initialized to "" by default
 	string previous;	// previous word
 	string current;		// current word
 	
 	while (cin >> current) {		// read a stream of words
+		++number_of_words;			// increment word count
+
 		if (previous == current)
-			cout << "repeated word: " << current << '\n';
+			cout << "word number " << number_of_words
+				<< " repeated: " << current << '\n';
 	
 	previous = current;
 	}
