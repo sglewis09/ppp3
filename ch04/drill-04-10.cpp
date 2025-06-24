73d7e5b03ef729ab58c01032ca59be290ca13dce
Stephen G. Lewis <sglewis@sglewis.com>
2025-06-24 08:02:32 -0400

    Added chapter 04 drill 10 (with errors)

---
+++ ch04/drill-04-10.cpp
@@ -0,0 +1,32 @@
+// Programming: Principles and Practice Using C++ - 3rd Edition
+// by Bjarne Stroustrup
+//
+// Drill 3.0
+// Page 110
+//
+// Scaffold for chapter 4 drills
+
+//--- Comment out and use include until I figure how to compile with modules
+// import std;
+#include "PPPheaders.h"
+
+int main() {
+  try {
+    string s = "ape";
+
+    if (s == "fool")
+      cout << "Success!\n";
+
+    return 0;
+  }
+
+  catch (exception &e) {
+    cerr << "error: " << e.what() << endl;
+    return 1;
+  }
+
+  catch (...) {
+    cerr << "Oops: unknown excetption" << endl;
+    return 2;
+  }
+}
